4bf6592d37c59dc0e91e593db0c2d4d5b319331b
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-11-23 11:43:07 -0500

    models are working

--- models/league.js
+++ models/league.js
@@ -1,3 +1,18 @@
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.js")[env];
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
+  }
+
 module.exports = function(sequelize, DataTypes) {
     var League = sequelize.define("League", {
       // Giving the League model a name of type STRING
--- models/user.js
+++ models/user.js
@@ -1,3 +1,18 @@
+var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.js")[env];
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
+  }
+
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
       name: DataTypes.STRING,
