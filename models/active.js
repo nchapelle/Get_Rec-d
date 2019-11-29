eb4a14785361442f730b966ef421a48cf343231d
Raleigh <53280832+jkoenig19@users.noreply.github.com>
2019-11-29 15:27:56 -0500

    Created connections between front end and back end

---
+++ models/active.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Active = sequelize.define('Active', {
+    name: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    zip: DataTypes.STRING,
+    team_id: DataTypes.STRING,
+    touchdown: DataTypes.STRING,
+    goals: DataTypes.STRING,
+    score: DataTypes.STRING
+  });
+  return Active;
+};
--- models/user.js
+++ models/user.js
@@ -1,62 +1,73 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-    userName: {
-      type: DataTypes.STRING,
-      //only letters
-      is: ["^[a-z]+$", 'i'],
-      //between 3 and 25 chars
-      len: [3, 25],
-      notEmpty: true,
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      //only letters
-      is: ["^[a-z]+$", 'i'],
-      //between 2 and 25 chars
-      len: [2, 25],
-      notEmpty: true,
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      //only letters
-      is: ["^[a-z]+$", 'i'],
-      //between 2 and 25 chars
-      len: [2, 25],
-      notEmpty: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      notEmpty: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      lowercase: true,
-      validate: {
-        isEmail: true,
-        notEmpty: true
-      }
-    },
-    phone: {
-      type: DataTypes.STRING,
-      isNumeric: true,
-      len: [10, 11],
-      notEmpty: true
-    },
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    zip: DataTypes.STRING,
+    // name: {
+    //   type: DataTypes.STRING,
+    //   //only letters
+    //   is: ["^[a-z]+$", 'i'],
+    //   //between 3 and 25 chars
+    //   len: [3, 25],
+    //   notEmpty: true,
+    // },
+    // firstName: {
+    //   type: DataTypes.STRING,
+    //   //only letters
+    //   is: ["^[a-z]+$", 'i'],
+    //   //between 2 and 25 chars
+    //   len: [2, 25],
+    //   notEmpty: true,
+    // },
+    // lastName: {
+    //   type: DataTypes.STRING,
+    //   //only letters
+    //   is: ["^[a-z]+$", 'i'],
+    //   //between 2 and 25 chars
+    //   len: [2, 25],
+    //   notEmpty: true,
+    // },
+    // password: {
+    //   type: DataTypes.STRING,
+    //   notEmpty: true,
+    // },
+    // email: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   unique: true,
+    //   lowercase: true,
+    //   validate: {
+    //     isEmail: true,
+    //     notEmpty: true
+    //   }
+    // },
+    // phone: {
+    //   type: DataTypes.STRING,
+    //   isNumeric: true,
+    //   len: [10, 11],
+    //   notEmpty: true
+    // },
 
-    zip: {
-      type: DataTypes.STRING,
-      isNumeric: true,
-      len: [5],
-    }
+    // zip: {
+    //   type: DataTypes.STRING,
+    //   isNumeric: true,
+    //   len: [5],
+    // },
+    team_id: DataTypes.STRING,
+    touchdowns: DataTypes.STRING,
+    goals: DataTypes.STRING,
+    score: DataTypes.STRING
   }, {});
-  User.associate = function (models) {
-    // associations can be defined here
-    User.belongsToMany(models.team, { as: 'Teams', through: 'teamUserXR' })
-    User.hasMany(models.message)
+  // User.associate = function (models) {
+  //   // associations can be defined here
+  //   User.belongsToMany(models.team, { as: 'Teams', through: 'teamUserXR' })
+  //   User.hasMany(models.message)
 
-  };
+  // };
   return User;
 };
